952f92b0df4f7ac9f6636d7075efbff9f7f3292f
Sezayir Dağtekin <sezayir@hotmail.com>
2022-11-27 21:50:10 +0300

    call order client

--- grpc/shopping/src/main/java/com/shopping/service/UserServiceImpl.java
+++ grpc/shopping/src/main/java/com/shopping/service/UserServiceImpl.java
@@ -1,16 +1,27 @@
 package com.shopping.service;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.shopping.dao.UserDao;
 import com.shopping.entity.User;
+import com.shopping.order.client.OrderClient;
+import com.shopping.server.OrderServer;
+import com.shopping.stub.order.Order;
 import com.shopping.stub.user.Gender;
 import com.shopping.stub.user.UserRequest;
 import com.shopping.stub.user.UserResponse;
 import com.shopping.stub.user.UserServiceGrpc.UserServiceImplBase;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
 public class UserServiceImpl extends UserServiceImplBase {
-
+	private static final Logger logger = Logger.getLogger(UserServiceImpl.class.getName());
+			
 	private UserDao userDao = new UserDao();
 	
 	@Override
@@ -24,12 +35,31 @@ public class UserServiceImpl extends UserServiceImplBase {
 		.setId(user.getId())
 	    .setGender(Gender.valueOf(user.getGender()))
 	    .build();
+
+		getOrders(userResponse);
 		
 		responseObserver.onNext(userResponse); 
 		responseObserver.onCompleted(); //Ensure rpc call complete
 		
 		
 
+	}
+
+	private void getOrders(UserResponse userResponse) {
+		ManagedChannel channel= ManagedChannelBuilder
+								.forTarget("localhost:5002")
+								.usePlaintext().build();
+		
+		OrderClient orderClient = new OrderClient(channel);
+		List<Order> order = orderClient.getOrders(userResponse.getId());
+		
+		//If you do not  have multiple call shut down channel. Otherwise keep channel open
+		
+		try {
+			channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			logger.log(Level.SEVERE, "Channel did not shutdown", e);
+		}
 	}
 
 }
